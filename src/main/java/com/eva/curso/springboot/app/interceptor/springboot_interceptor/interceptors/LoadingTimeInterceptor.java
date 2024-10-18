b5c97428636b89905b2bc0360cc2959b75ffa9e7
EvaMsanchez <eva_crm20@hotmail.com>
2024-10-18 12:44:33 +0200

    Implementando interceptor TiempoTranscurrido

--- src/main/java/com/eva/curso/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/eva/curso/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,7 @@
 package com.eva.curso.springboot.app.interceptor.springboot_interceptor.interceptors;
 
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
@@ -17,22 +19,38 @@ public class LoadingTimeInterceptor implements HandlerInterceptor
     // Constante para el Log
     private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
 
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-            @Nullable ModelAndView modelAndView) throws Exception {
-        
-        logger.info("LoadingTimeInterceptor: postHandler() saliendo ...." + ((HandlerMethod)handler).getMethod().getName());
-    }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        
-        HandlerMethod controller = ((HandlerMethod)handler);
 
-        logger.info("LoadingTimeInterceptor: preHandler() entrando ...." + controller.getMethod().getName()); // concatener el nombre del método que se está invocando
+        // Objeto del controlador
+        HandlerMethod controller = ((HandlerMethod)handler);
+        logger.info("LoadingTimeInterceptor: preHandler() entrando ...." + controller.getMethod().getName()); // concatener el nombre del método que se está invocando a través de la URL
                 
+        long start = System.currentTimeMillis(); // fecha inicial del sistema
+        request.setAttribute("start", start);
+
+        // Simular una tardanza o demora de tiempo, para hacer luego la comprobación de cuanto tarda en cargar la página
+        Random random = new Random();
+        int delay = random.nextInt(500);
+        Thread.sleep(delay);
         return true; 
     }
+
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable ModelAndView modelAndView) throws Exception {
+        
+        long end = System.currentTimeMillis(); // fecha final del sistema
+        long star = (long) request.getAttribute("start"); // cogemos la fecha inicial
+        long result = end - star;
+        logger.info("Tiempo transcurrido:" + result + "milisegundos!");
+
+        logger.info("LoadingTimeInterceptor: postHandler() saliendo ...." + ((HandlerMethod)handler).getMethod().getName());
+    }
+
+   
 
 }
