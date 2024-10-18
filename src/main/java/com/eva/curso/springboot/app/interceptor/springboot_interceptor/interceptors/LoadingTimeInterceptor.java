837a993de5f9ee031953ace0036d4b5b744a3a68
EvaMsanchez <eva_crm20@hotmail.com>
2024-10-18 13:57:38 +0200

    Retornando false en un interceptor

--- src/main/java/com/eva/curso/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/eva/curso/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,5 +1,8 @@
 package com.eva.curso.springboot.app.interceptor.springboot_interceptor.interceptors;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import org.slf4j.Logger;
@@ -9,6 +12,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -36,6 +41,19 @@ public class LoadingTimeInterceptor implements HandlerInterceptor
         int delay = random.nextInt(500);
         Thread.sleep(delay);
         return true; 
+
+        // Si retornamos false en un interceptor
+        /* Map<String, String> json = new HashMap<>();
+        json.put("error", "no tienes acceso a esta página!");
+        json.put("date", new Date().toString());
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsoString = mapper.writeValueAsString(json);
+        response.setContentType("application/json");
+        response.setStatus(401);
+        response.getWriter().write(jsoString);
+        return false;
+        */
     }
 
 
